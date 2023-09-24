a0296825952eda37d4cd22ea21d8189e5246abf4
CS21B008 <cs21b008@iittp.ac.in>
2023-09-24 22:31:59 +0530

    updated layout

--- Alexis/src/Header.js
+++ Alexis/src/Header.js
@@ -3,28 +3,30 @@ import './Styles/header.css';
 function Header({ setNavId }){
     return(
       <header className="App-header">
+        <div className='nav-header'>
+          <div className="Home-header">
+            <button onClick={() => setNavId(0)}>
+              <span>Home</span>
+            </button>
+          </div>
+          <div className='About-header'>
+            <button onClick={() => setNavId(1)}>
+              About
+            </button>
+          </div>
+          <div className='Contact-header'>
+            <button onClick={() => setNavId(2)}>
+              Contact
+            </button>
+          </div>
+          <div className='Login-header'>
+            <button onClick={() => setNavId(3)}>
+              Login/Signup
+            </button>
+          </div>
+        </div>
         <div className='Logo-header'>
-          <p>Alexis</p>
-        </div>
-        <div className="Home-header">
-          <button onClick={() => setNavId(0)}>
-            <span>Home</span>
-          </button>
-        </div>
-        <div className='About-header'>
-          <button onClick={() => setNavId(1)}>
-            About
-          </button>
-        </div>
-        <div className='Contact-header'>
-          <button onClick={() => setNavId(2)}>
-            Contact
-          </button>
-        </div>
-        <div className='Login-header'>
-          <button onClick={() => setNavId(3)}>
-            Login/Signup
-          </button>
+          <p className='Logo-para'>Alexis</p>
         </div>
       </header>
     );
