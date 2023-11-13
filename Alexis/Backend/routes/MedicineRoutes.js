219a9b3b3f3b2625cdcf3f6ffa64b805d7ae3d2f
VCHARVY <cs21b054@iittp.ac.in>
2023-11-13 18:13:14 +0530

    re

--- Alexis/Backend/routes/MedicineRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { postMediData,postPermit} = require('../controllers/MedicineControl');
-router.route('/mediinfo').post(postMediData);
-router.route('/permit').post(postPermit);
-module.exports = router;
--- Alexis/Backend/routes/dataRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { postData,postPatient } = require('../controllers/dataControl');
-router.route('/user').post(postData);
-router.route('/patient').post(postPatient);
-module.exports = router;
